f99f8977e9725eb96b626133c8b66e8f35487772
FELIPE_AGUIAR <felipeferreiraaguiar9@gmail.com>
2024-08-07 16:02:29 -0400

    feat: Controller

--- src/main/java/com/example/gaia/controller/UserController.java
+++ src/main/java/com/example/gaia/controller/UserController.java
@@ -2,10 +2,13 @@ package com.example.gaia.controller;
 
 import com.example.gaia.dto.CreateUserDTO;
 import com.example.gaia.dto.LoginDTO;
+import com.example.gaia.dto.http_response.SignInResponse;
+import com.example.gaia.dto.http_response.SignUpResponse;
 import com.example.gaia.entity.User;
+import com.example.gaia.errors.ApplicationError;
 import com.example.gaia.repository.UserRepository;
 import com.example.gaia.service.PasswordService;
-import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,27 +28,34 @@ public class UserController {
   }
 
   @PostMapping("/sign-up")
-  public ResponseEntity<String> createUser(@RequestBody CreateUserDTO userDTO) {
-    User newUser = new User(userDTO);
-    newUser.setPassword(passwordService.getHashedPassword(newUser.getPassword()));
+  public ResponseEntity createUser(@RequestBody CreateUserDTO userDTO) throws ApplicationError {
+    try {
+      User newUser = new User(userDTO);
+      newUser.setPassword(passwordService.getHashedPassword(newUser.getPassword()));
 
-    userRepository.save(newUser);
+      userRepository.save(newUser);
 
-    return new ResponseEntity<>("User created", HttpStatus.CREATED);
+      SignUpResponse response = new SignUpResponse("Usuário criado com sucesso", HttpStatus.CREATED, "token");
+      return ResponseEntity.status(response.getStatus()).body(response);
+    } catch (DataIntegrityViolationException exception) {
+      throw new ApplicationError("Nome de usuário já em uso", 400, null);
+    }
   }
 
   @PostMapping("/sign-in")
-  public ResponseEntity<String> signIn(@RequestBody LoginDTO loginDTO) {
+  public ResponseEntity signIn(@RequestBody LoginDTO loginDTO) throws ApplicationError {
+
     User user = userRepository.findByUsername(loginDTO.getUsername());
 
     if (user == null) {
-      throw new EntityNotFoundException("Nenhum usuário foi encontrado com este login");
+      throw new ApplicationError("", 404, null);
     }
 
     if (passwordService.matchPassword(loginDTO.getPassword(), user.getPassword())) {
-      return new ResponseEntity<>("Login successful", HttpStatus.OK);
+      throw new ApplicationError("", 404, null);
     }
 
-    return new ResponseEntity<>("Invalid username or password", HttpStatus.UNAUTHORIZED);
+    SignInResponse response = new SignInResponse("Usuário logado com sucesso", HttpStatus.OK, "token");
+    return ResponseEntity.status(response.getStatus()).body(response);
   }
 }
